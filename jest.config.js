8ea126a459bbdaa5f57ecd16c979de2c7b2a94d0
Philip Davis <phil@decembersoft.com>
2021-07-04 16:03:15 -0400

    Ignore non-source directories when testing in watch mode

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  watchPathIgnorePatterns: [
+    "<rootDir>/.vscode/",
+    "<rootDir>/data/",
+    "<rootDir>/node_modules/",
+    "<rootDir>/test/"
+  ]
 };
