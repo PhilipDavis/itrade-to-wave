8c95295fe5895d98a702b5e29850efac72adb084
Philip Davis <phil@decembersoft.com>
2021-07-05 16:28:45 -0400

    Added logging and a little polish

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as dotenv from 'dotenv';
 import { StateManager } from './transactions/StateManager';
+import { toCsv } from './transactions/toCsv';
 import { TransactionType } from './transactions/transaction';
 import { TransactionProcessor } from './transactions/TransactionProcessor';
 import { WaveDriver } from './wave/WaveDriver';
@@ -30,34 +31,50 @@ const batchSize = parseInt(loadEnv('BATCH_SIZE'), 10);
 (async () => {
     let exitCode = 0;
 
+    console.log('Loading data...');
     const stateManager = await StateManager.loadFromDisk(transactionsCsvFilename, holdingsJsonFilename);
 
+    console.log('Launching browser...');
     const waveDriver = await WaveDriver.launch();
     try {
+        console.log('Logging into Wave...');
         await waveDriver.login(login, password);
+
+        console.log('Navigating to the Transactions page...');
         const txPage = await waveDriver.loadTransactionsPage();
 
         const txProcessor = new TransactionProcessor(txPage, cashAccountName, equitiesAccountName);
 
+        console.log('Ready to begin');
+        let processedCount = 0;
         while (true) {
             const done = await stateManager.withNextTransaction(async (tx, holding) => {
                 tx.type === TransactionType.CashDiv
                     ? console.log(`Processing ${tx.type} ${tx.symbol} $${tx.settlementAmount}...`)
                     : console.log(`Processing ${tx.type} ${tx.qty} ${tx.symbol}...`);
-                return await txProcessor.process(tx, holding);
+                
+                try {
+                    return await txProcessor.process(tx, holding);
+                }
+                catch (err) {
+                    throw new Error(`Failed on "${toCsv(tx).trim()}": ${err.message}`);
+                }
             });
             if ((++processedCount >= batchSize && batchSize > 0) || done) {
                 break;
             }
         }
+        console.log(`Processed ${processedCount} transactions`);
     }
     catch (err) {
         console.error(err.message);
         exitCode = 1;
     }
     finally {
+        console.log('Closing browser...');
         await waveDriver.close();
     }
 
+    console.log(`Exiting with code ${exitCode}`);
     process.exit(exitCode);
 })();    
